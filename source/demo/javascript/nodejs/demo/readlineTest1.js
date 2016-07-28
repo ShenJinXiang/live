51fd828daa70f92f24dfb199f1f7701b39cbae07
shenjinxiang <sjx-sword@163.com>
2016-07-28 15:55:08 +0800

    readline write txt

---
+++ source/demo/javascript/nodejs/demo/readlineTest1.js
@@ -0,0 +1,22 @@
+var readline = require('readline');
+var fs = require('fs');
+var os = require('os');
+
+var fReadName = './001.js';
+var fWriteName = './001.txt';
+var fRead = fs.createReadStream(fReadName);
+var fWrite = fs.createWriteStream(fWriteName);
+
+var objReadLine = readline.createInterface({
+	input: fRead
+});
+
+objReadLine.on('line', function(line){
+	fWrite.write(line + os.EOL);
+});
+
+objReadLine.on('close', function(){
+	console.log("end...........");
+});
+
+
