25b1977872790fe7146620fe8fb03041951497d4
shenjinxiang <sjx-sword@163.com>
2016-08-01 09:49:16 +0800

    console test2

---
+++ source/demo/javascript/nodejs/demo/console1.js
@@ -0,0 +1,25 @@
+var flag = true;
+var NUM = 30;
+var index = 1;
+setInterval(function(){
+	var str1 = '';
+	var str2 = '';
+	for(var i = 0; i < (NUM - index); i++) {
+		str1 += ' ';
+	}
+	for(var i = 0; i < (2 * index) - 1; i++) {
+		str2 += '*';
+	}
+	console.log(str1 + str2);
+	if(index >= NUM) {
+		flag = false;
+	}
+	if(index <= 1) {
+		flag = true;
+	}
+	if(flag) {
+		index++;
+	} else {
+		index--;
+	}
+}, 100 * 1);
