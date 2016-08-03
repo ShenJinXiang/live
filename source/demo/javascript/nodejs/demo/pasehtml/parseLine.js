af06ca17e458dafa4fdad93a8b61920cbf9dd915
shenjinxiang <sjx-sword@163.com>
2016-08-03 08:10:57 +0800

    parseline

--- source/demo/javascript/nodejs/demo/pasehtml/parseLine.js
+++ source/demo/javascript/nodejs/demo/pasehtml/parseLine.js
@@ -32,3 +32,47 @@ function h1(line) {
 	line = line.replace(space, '');
 	return '<h1>' + line.substr(1)+ '</h1>' + os.EOL;
 }
+
+function hr(line) {
+	return '<hr/>' + os.EOL;
+}
+
+function b(line) {
+	line = line.replace(space, '');
+	return '<b>' + line.substr(2, line.length - 2)+ '</b>' + os.EOL;
+}
+
+function ul(line) {
+	line = line.replace(space, '');
+	if(codeStatus == constant.mdStatus['default']) {
+		condeStatus = constant.mdStatus['ul'];
+		return '<ul>' + os.EOL + 
+			'<li>' + line.substr(1) + '</li>' + os.EOL
+	} else (codeStatus == constant.mdStatus['ul']) {
+		return '<li>' + line.substr(1) + '</li>' + os.EOL;
+	}
+}
+
+function code(line) {
+	line = line.replace(space, '');
+	if(codeStatus == constant.mdStatus['default']) {
+		codeStatus = constant.mdStatus['code'];
+		var language = line.substr(3);
+		if(constant.language.indexOf(language) >= 0) {
+			return '<pre class="line-numbser language-' + language + '">' + os.EOL + '<code>' + os.EOL;
+		} else {
+			return '<pre class="line-number result-code">' + os.EOL;
+		}
+	} else if(codeStatus == constant.mdStatus['code']) {
+		return '</code>' + os.EOL + '</pre>' + os.EOL;
+	}
+}
+
+function defaultLine(line) {
+	return '<p>' + line + '</p>' + os.EOL;
+}
+
+function emptyLine(line) {
+	codeStatus = constant.mdStatus['default'];
+	return '<br>' + os.EOL;
+}
