689a1ccfb2f7da93c8c4790d725bda8a07703751
shenjinxiang <sjx-sword@163.com>
2016-08-04 09:20:13 +0800

    clear

--- source/demo/javascript/nodejs/demo/pasehtml/constantTest.js
+++
@@ -1,9 +0,0 @@
-var reg = require('./constant').regular;
-
-var str = ' ## 安守本分';
-
-for(var r in reg) {
-	if(reg[r].test(str)) {
-		console.log(str, r, reg[r]);
-	}
-}
--- source/demo/javascript/nodejs/demo/pasehtml/readLinesTest.js
+++
@@ -1,6 +0,0 @@
-var readLines = require('./readLines');
-
-var fileName = './readLines.js';
-readLines(fileName, function(data){
-	console.log(data.join('\n'));
-});
