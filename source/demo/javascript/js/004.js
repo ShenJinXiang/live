439f6731724f4aa16979ef2467f24c618713b97a
shenjinxiang <sjx-sword@163.com>
2016-08-05 07:44:28 +0800

    sdfds

--- source/demo/javascript/js/004.js
+++ source/demo/javascript/js/004.js
@@ -9,3 +9,26 @@ function extend(o, p) {
 	}
 	return o;
 }
+
+/**
+ * 将p中的可枚举属性复制到o中，并返回o
+ * 如果o和p中有同名的属性，o中的属性将不受影响
+ * 这个函数并不处理getter和setter以及复制属性
+ */
+function merge(o, p) {
+	for(prop in p) {
+		if(o.hasOwnProperty[prop]) continue;
+		o[prop] = p[prop];
+	}
+	return o;
+}
+
+/**
+ * 如果o中的属性在p中没有同名属性，则从o中删除这个属性，并返回o
+ */
+function restrict(o, p) {
+	for(prop in o) {
+		if(!(prop in p)) delete o[prop];
+	}
+	return o;
+}
