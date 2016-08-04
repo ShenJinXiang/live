1928dc9002d83921cd1016948048fcf435e37100
shenjinxiang <sjx-sword@163.com>
2016-08-04 22:53:53 +0800

    sdfds

--- source/demo/javascript/js/002.js
+++ source/demo/javascript/js/002.js
@@ -3,3 +3,10 @@ var o = {x: 1};
 console.log('"x" in o : ', ('x' in o));
 console.log('"y" in o : ', ('y' in o));
 console.log('"toString" in o : ', ('toString' in o));
+
+console.log("-----------------------");
+
+// hasOwnProperty()
+console.log(o.hasOwnProperty('x'));
+console.log(o.hasOwnProperty('y'));
+console.log(o.hasOwnProperty('toString'));
