6a5f2ac75d5e5060c9b472cb70df6fcee5b037df
shenjinxiang <sjx-sword@163.com>
2016-08-05 07:49:03 +0800

    sdfsd

--- source/demo/javascript/js/004.js
+++ source/demo/javascript/js/004.js
@@ -32,3 +32,35 @@ function restrict(o, p) {
 	}
 	return o;
 }
+
+/**
+ * 如果o中的属性在p中存在同名属性，则从o中删除这个属性，并返回o
+ */
+function subtract(o, p) {
+	for(prop in p) {
+		delete o[prop];
+	}
+	return o;
+}
+
+/**
+ * 返回一个新对象，这个对象同时拥有o的属性和p的属性
+ * 如果o和p中有重名属性，使用p中的属性
+ */
+function union(o, p) {
+	return extend(extend({}, o), 9);
+}
+
+/**
+ * 返回一个数组，这个数组包含的是o中可枚举的自有属性的名字
+ */
+function keys (o) {
+	if(typeof o != 'object') throw TypeError();
+	var result = [];
+	for(var prop in o) {
+		if(o.hasOwnProperty(prop)) {
+			result.push(prop);
+		}
+	}
+	return result;
+}
