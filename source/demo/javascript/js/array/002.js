6d0090fd0ca47fb5c82f2a922212b73eb2e25996
shenjinxiang <sjx-sword@163.com>
2016-08-06 08:57:25 +0800

    array length

---
+++ source/demo/javascript/js/array/002.js
@@ -0,0 +1,14 @@
+/*
+var a = [];
+a[-1] = 0;
+console.log(a);
+console.log(a.length);
+*/
+var a = [1, 2, 3, 4, 5];
+console.log(a);
+a.length = 3;
+console.log(a);
+a.length = 0;
+console.log(a);
+a.length = 5;
+console.log(a);
