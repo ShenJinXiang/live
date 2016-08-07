65efa188dc379075156e3b916a23fe3154c03a58
shenjinxiang <sjx-sword@163.com>
2016-08-07 20:35:51 +0800

    数组类型

---
+++ source/demo/javascript/js/array/024.js
@@ -0,0 +1,10 @@
+console.log('Array.isArray([]): ', Array.isArray([]));
+console.log('Array.isArray({}): ', Array.isArray({}));
+
+console.log('[] instanceof Array: ', ([] instanceof Array));
+console.log('{} instanceof Array: ', (({}) instanceof Array));
+
+console.log('type of []: ', typeof []);
+console.log('type of {}: ', typeof {});
+
+
