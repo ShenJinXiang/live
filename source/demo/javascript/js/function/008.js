4da0b472415ced9ebba2beabe8e7ec309ed9b718
shenjinxiang <sjx-sword@163.com>
2016-08-08 22:33:42 +0800

    sdfsd

---
+++ source/demo/javascript/js/function/008.js
@@ -0,0 +1,11 @@
+uniqueInteger.counter = 0;
+function uniqueInteger() {
+	return uniqueInteger.counter++;
+}
+
+console.log(uniqueInteger());
+console.log(uniqueInteger());
+console.log(uniqueInteger());
+console.log(uniqueInteger());
+console.log(uniqueInteger());
+console.log(uniqueInteger());
