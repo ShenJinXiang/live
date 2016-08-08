eac540d83ce0967782ac8d209e839f128c5b1aa7
shenjinxiang <sjx-sword@163.com>
2016-08-08 23:22:34 +0800

    sdfdsf

---
+++ source/demo/javascript/js/function/015.js
@@ -0,0 +1,18 @@
+function counter() {
+	var n = 0;
+	return {
+		count: function() {return n++;},
+		reset: function() {n = 0;}
+	};
+}
+
+var c = counter();
+var d = counter();
+console.log(c.count());
+console.log(c.count());
+console.log(d.count());
+console.log(c.count());
+console.log(d.count());
+d.reset();
+console.log(c.count());
+console.log(d.count());
