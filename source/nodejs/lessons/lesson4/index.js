777a7c8bc6b35deebfddb9c5ebe5264e007c0be0
shenjinxiang <sjx-sword@163.com>
2016-11-07 11:40:53 +0800

    lesson4

---
+++ source/nodejs/lessons/lesson4/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+var eventproxy = require('eventproxy');
+
+var url = require('url');
+
+var cnodeUrl = 'https://cnodejs.org/';
+
+var app = express();
+
+app.get('/', function (request, response) {
+	superagent.get(cnodeUrl).end(function (err, res) {
+		if (err) {
+			console.error(err);
+		}
+
+		var topicUrls = [];
+		var $ = cheerio.load(res.text);
+		$('#topic_list .topic_title').each(function (index, element) {
+			var $element = $(element);
+			var href = url.resolve(cnodeUrl, $element.attr('href'));
+			topicUrls.push(href);
+		});
+		// response.send(topicUrls);
+
+		var ep = new eventproxy();
+
+		ep.after('topic_html', topicUrls.length, function (topics) {
+			topics = topics.map(function (topicPair) {
+				var topicUrl = topicPair[0];
+				var topicHtml = topicPair[1];
+				var $ = cheerio.load(topicHtml);
+				return ({
+					title: $('.topic_full_title').text().trim(),
+					href: topicUrl,
+					comment1: $('.reply_content').eq(0).text().trim(),
+				});
+			});
+			response.send(topics);
+		});
+
+		topicUrls.forEach(function (topicUrl) {
+			superagent.get(topicUrl).end(function (err, res) {
+				console.log('fetch', topicUrl, 'successful');
+				ep.emit('topic_html', [topicUrl, res.text]);
+			})
+		});
+	});
+});
+
+app.listen(3000, function () {
+	console.log('app running at 3000 port.');
+});
