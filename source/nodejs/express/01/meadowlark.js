b03ca8cb78c8aa19aa0b306e0426b19803d7d1ba
shenjinxiang <sjx-sword@163.com>
2016-11-08 16:55:56 +0800

    ch4

--- source/nodejs/express/01/meadowlark.js
+++ source/nodejs/express/01/meadowlark.js
@@ -1,9 +1,11 @@
 // meadowlark.js
 
 var express = require('express');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var fortune = require('./lib/fortune.js');
+
 var app = express();
 
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -19,19 +21,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-	var fortunes = [
-		"Conquer your fears or they will conquer you.",
-		"Rivers need springs.",
-		"Do not fear what you don't know.",
-		"You will have a pleasant surprise.",
-		"Whenever possible, keep it simple.",
-	];
 	/*
 	res.type('text/plain');
 	res.send('About Meadowlark Travel');
 	*/
-	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about', {fortune: randomFortune});
+	res.render('about', {fortune: fortune.getFortune()});
 });
 
 // 定制404页面
