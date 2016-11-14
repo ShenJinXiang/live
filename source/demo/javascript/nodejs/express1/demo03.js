faba79a7dd33991fb70cf98830162fe1bb3a0039
shenjinxiang <sjx-sword@163.com>
2016-11-14 22:37:30 +0800

    df

--- source/demo/javascript/nodejs/express1/demo03.js
+++ source/demo/javascript/nodejs/express1/demo03.js
@@ -1,17 +1,9 @@
 var str = 'insert into user values (#{name }, #{password}, #{ address}, #{ age })';
 
 var reg1 = /(\#\{){1}\s*\w+\s*\}{1}/g
-
-// var reg2 = /(?i)(?<=\#\{)((?!\#\{).)*(?=\})/g
-
-var params = str.match(reg1);
 var reg2 = /^\s+|\s+$/g
-console.log(params);
+var params = str.match(reg1);
 var arr = params.map(function (item, index) {
-	// console.log(item, index);
-
-	var re =  item.substring(2, item.length - 1);
-	
-	return re.replace(reg2, '');
+	return item.substring(2, item.length - 1).replace(reg2, '');
 });
 console.log(arr);
