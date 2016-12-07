892c7515f888f4752e18d48f718c1c6137d8739e
shenjinxiang <sjx-sword@163.com>
2016-12-07 18:07:03 +0800

    sdf

---
+++ source/java/maven/demo1/src/main/java/com/qysoft/demo1/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.qysoft.demo1;
+
+public class HelloWorld {
+
+	public String sayHello () {
+		return "Hello ShenJinXiang";
+	}
+
+	public static void main (String[] args) {
+		System.out.print(new HelloWorld().sayHello());
+	}
+}
