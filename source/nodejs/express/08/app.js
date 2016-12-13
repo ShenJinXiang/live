9abd84e9c300633efdde3038f8f91539d0713d62
shenjinxiang <sjx-sword@163.com>
2016-12-13 23:10:32 +0800

    sdf

--- source/nodejs/express/08/app.js
+++ source/nodejs/express/08/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const path = require('path');
 
 let app = express();
 
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
+
+app.get('/user', function (req, res) {
+	let obj = {
+		'data' : {
+			'name': 'shenjinxiang',
+			'age': 30,
+			'address': '太原'
+		}
+	};
+	res.render('index', obj);
+});
 
 app.listen(3000, function () {
 	console.log('Server running at 3000 port.');
