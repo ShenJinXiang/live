9a2fdc842ae5dcbe90444115befabad708ad6f63
shenjinxiang <sjx-sword@163.com>
2017-01-02 14:35:10 +0800

    js test

---
+++ source/demo/javascript/js/demo/005.js
@@ -0,0 +1,6 @@
+let a = [1, 2, 3];
+let b = a.concat([4, 5], [6, 7]);
+let c = a.concat(4, [5, [6, 7]]);
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ source/demo/javascript/js/demo/006.js
@@ -0,0 +1,5 @@
+let arr = [11, 22, 33, 44, 55, 66, 77, 88];
+let index = arr.indexOf('22');
+console.log(arr);
+console.log(index);
+console.log(arr[index])
