ca07c2b232b6affada8f74a82842c7398db95af8
shenjinxiang <sjx-sword@163.com>
2017-02-08 17:49:17 +0800

    zq

--- source/canvas/zq/index.js
+++ source/canvas/zq/index.js
@@ -2,13 +2,17 @@
 	let canvas = document.getElementById('canvas');
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
+	//canvas.width = 1200;
+	//canvas.height = 800;
 	let context = canvas.getContext('2d');
 
 	let img = new Image();
-	img.src = './timg.jpeg';
+	//img.src = './timg.jpeg';
+	img.src = './zq02.png';
 	img.onload = function () {
 		var p = context.createPattern(img, 'repeat');
 		context.fillStyle = p;
 		context.fillRect(0, 0, canvas.width, canvas.height);
+		//context.drawImage(img, 0, 0, canvas.width, canvas.height);
 	};
 })();
