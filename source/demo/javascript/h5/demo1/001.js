b4d318de74d304ebb1116ba563e12c6192fbecbb
shenjinxiang <sjx-sword@163.com>
2017-02-24 18:38:04 +0800

    video

---
+++ source/demo/javascript/h5/demo1/001.js
@@ -0,0 +1,33 @@
+(function () {
+	var video = document.getElementById('myvideo');
+	video.src = '../oggs/001.ogg';
+	video.width = 600;
+	video.height = 400;
+
+	var jindu = document.getElementById('jindu');
+	jindu.min = 0;
+	jindu.value = 0;
+
+	video.onloadedmetadata = function () {
+		var initialTime = video.initialTime;
+		var duration = video.duration;
+		jindu.max = duration;
+	
+		console.log(initialTime);
+		console.log(duration);
+	};
+
+	jindu.onmousemove = function () {
+		var jd = jindu.value;
+		video.currentTime = jd;
+	}
+	var play = document.getElementById('play');
+	var pause = document.getElementById('pause');
+
+	play.onclick = function () {
+		video.play();
+	};
+	pause.onclick = function () {
+		video.pause();
+	};
+})();
