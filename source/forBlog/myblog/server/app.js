1d3f3b68b87d328dbf32e476a837450dff7d00ad
shenjinxiang <sjx-sword@163.com>
2017-03-27 15:50:27 +0800

    for my blog

---
+++ source/forBlog/myblog/server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(getStaticPath()));
+
+app.listen(4000, function() {
+	console.log('Server running at 4000 port.');
+});
+
+function getStaticPath() {
+	if (process.argv.length <= 2) {
+		return path.join(__dirname, '../');
+	}
+	if (!!process.argv[2]) {
+		return process.argv[2];
+	}
+	return path.join(__dirname, '../');
+}
