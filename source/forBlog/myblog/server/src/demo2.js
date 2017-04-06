03b64ff34bf0c2a67b0a0eb9871b449879d2145a
shenjinxiang <sjx-sword@163.com>
2017-04-06 10:32:56 +0800

    style

---
+++ source/forBlog/myblog/server/src/demo2.js
@@ -0,0 +1,12 @@
+var mkdirs = module.exports.mkdirs = function(dirpath, mode, callback) {
+	path.exists(dirpath, function(exists) {
+		if(exists) {
+			callback(dirpath);
+		} else {
+			//尝试创建父目录，然后再创建当前目录
+			mkdirs(path.dirname(dirpath), mode, function(){
+				fs.mkdir(dirpath, mode, callback);
+			});
+		}
+	});
+};
