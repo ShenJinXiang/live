efe5688d9a8f7fb992aac5a12cf8373a060b3fdc
shenjinxiang <sjx-sword@163.com>
2017-04-11 21:27:33 +0800

    log

--- source/forBlog/myblog/server/src/log.js
+++ source/forBlog/myblog/server/src/log.js
@@ -1,7 +1,7 @@
 const dateUtil = require('./dateUtil');
 
 let log = function(info) {
-	let dateStr = dateUtil.getDateStr(new Date(), 'yyyy-MM-dd HH:mm:ss');
+	let dateStr = dateUtil.getDateStr(new Date(), 'yyyy-MM-dd HH:mm:ss, SSS');
 	console.log(`[${dateStr}] ${info}`);
 }
 
