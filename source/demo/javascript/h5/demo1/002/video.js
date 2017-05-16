8f8b02f294f5c6c6fe626e71f962449c61609f48
shenjinxiang <sjx-sword@163.com>
2017-05-16 11:53:48 +0800

    video demo 01

---
+++ source/demo/javascript/h5/demo1/002/video.js
@@ -0,0 +1,94 @@
+(function() {
+	var config = {
+		video_src: './001.mp4'
+	};
+	var video = document.createElement('video');
+	video.src = config.video_src;
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+	var mainDiv = document.getElementById('mainDiv');
+	var czDiv = document.getElementById('czDiv');
+	var bfzjd = document.getElementById('bfzjd');
+	var dqbfjd = document.getElementById('dqbfjd');
+	var bfzt = document.getElementById('bfzt');
+	var sjxx = document.getElementById('sjxx');
+	var timer;
+
+	addEvent(video, 'loadedmetadata', function() {
+		canvas.width = video.videoWidth;
+		canvas.height = video.videoHeight;
+		czDiv.style.width = video.videoWidth + 'px';
+		var duration = video.duration.toFixed(0);
+
+		timer = setInterval(function() {
+			setDqbfjd();
+			draw();
+		}, 20);
+	});
+
+	/**
+	addEvent(video, 'timeupdate', function() {
+		setDqbfjd();
+		draw();
+	});
+	*/
+
+	addEvent(bfzt, 'click', function(e) {
+		if (video.paused) {
+			video.play();
+			if (!video.paused) {
+				bfzt.className = 'zt';
+			}
+		} else {
+			video.pause();
+			if(video.paused) {
+				bfzt.className = 'bf';
+			}
+		}
+		setDqbfjd();
+	});
+
+	function draw() {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.drawImage(video, 0, 0, canvas.width, canvas.height);
+	}
+
+
+	function setDqbfjd() {
+		var w = canvas.width * video.currentTime / video.duration;
+		dqbfjd.style.width = w + 'px';
+	}
+
+	function addEvent(dom, eType, func) {
+		if (dom.addEventListener) {
+			dom.addEventListener(eType, function(e) {
+				func(e);
+			});
+		} else if (dom.attachEvent) {
+			dom.attachEvent('on' + eType, function(e) {
+				func(e);
+			});
+		} else {
+			dom['on' + eType] = function(e) {
+				func(e);
+			}
+		}
+	}
+
+	function second2Minute(second) {
+    var time = parseInt(second);
+    if (time < 60) {
+        return '0' + ':' + ((time < 10) ? ('0' + time) : time );
+    }
+    var s = (time % 60);
+    s = (s < 10) ? ('0' + s) : s;
+    var m = Math.floor(time / 60);
+    if (time < (60 * 60)) {
+        return m + ':' + s;
+    }
+    var h = Math.floor(m / 60);
+    m = m % 60;
+    m = (m < 10) ? ('0' + m) : m;
+    return h + ':' + m + ':' + s;
+	}
+})();
