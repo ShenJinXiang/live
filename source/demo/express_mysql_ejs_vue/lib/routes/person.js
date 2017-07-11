4b08f06a28b34df50229b0cec977e850e2140cfb
shenjinxiang <sjx-sword@163.com>
2017-07-11 18:10:07 +0800

    sdf

--- source/demo/express_mysql_ejs_vue/lib/routes/person.js
+++ source/demo/express_mysql_ejs_vue/lib/routes/person.js
@@ -16,7 +16,7 @@ router.get('/', function(req, res) {
 router.post('/list', function(req, res) {
 	try {
 		let data = fileDao.read(fname);
-		res.json({result: true, data: {size: data.length, list: data}});
+		res.json({result: true, data: data});
 	} catch (err) {
 		res.json({result: false, msg: err.message});
 	}
--- source/demo/express_mysql_ejs_vue/public/js/person/index.js
+++ source/demo/express_mysql_ejs_vue/public/js/person/index.js
@@ -1,8 +1,73 @@
-var personList = {size: 0, list: []}
+var data = {
+	personList: [],
+	person: {
+		id: '',
+		name: '',
+		sex: '男',
+		age: '',
+		address: ''
+	}
+}
 
-var personData = new Vue({
-	el: '#personList',
-	data: personList
+var mainData = new Vue({
+	el: '#mainData',
+	data: data,
+	methods: {
+		// 新增事件，弹出新增窗口
+		add: function() {
+			data.person = {
+				id:'',
+				name: '',
+				sex: '男',
+				age: '',
+				address: ''
+			};
+			openContent('新增人员', 700, 'personContent');
+		},
+		edit: function(id) {
+			data.person = {
+				id:'',
+				name: '',
+				sex: '男',
+				age: '',
+				address: ''
+			};
+			doPost('/person/find', {id: id}, function(result) {
+				if (result.result) {
+					data.person = result.data;
+					openContent('修改人员', 700, 'personContent');
+				} else {
+					alertMsg(result.msg);
+				}
+			});
+		},
+		save: function() {
+			var url = '/person/add';
+			if (data.person.id) {
+				url = '/person/edit';
+			}
+			doPost(url, data.person, function(result) {
+				if (result.result) {
+					initData();
+					closeLayer();
+				} else {
+					alertMsg(result.msg);
+				}
+			});
+		},
+		del: function(id) {
+			doPost('/person/delete', {id: id}, function(result) {
+				if (result.result) {
+					initData();
+				} else {
+					alertMsg(result.msg);
+				}
+			});
+		},
+		close: function() {
+			closeLayer();
+		}
+	}
 });
 $(function() {
 
@@ -11,10 +76,8 @@ $(function() {
 
 function initData() {
 	doPost('/person/list', {}, function(result) {
-		console.log(result);
 		if (result.result) {
-			personList = result.data;
-			console.log(personList);
+			data.personList = result.data;
 		} else {
 			alertMsg(result.msg);
 		}
